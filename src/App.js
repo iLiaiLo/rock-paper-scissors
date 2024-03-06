020d661f594f01563b1e902c5e26527397aed62c
Ilo Tech <ilo.nikatsadze@gmail.com>
2024-03-06 22:40:48 +0400

    added some changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState,useEffect} from "react";
+import data from "./Data";
+
 
 function App() {
+
+  const [clicked, setClicked] = useState(false);
+  const [myScore, setMyScore] = useState(0);
+  const [computerScore, setComputerScore] = useState(0);
+  const [myItem, setMyItem] = useState({});
+  const [computerItem, setComputerItem] = useState({});
+  const [text,setText]=useState("");
+
+  useEffect(() => {
+    determineScore(myItem, computerItem);
+  }, [myItem,computerItem]);
+
+  const handleClick = (item) => {
+    setMyItem(item);
+    setClicked(true);
+    generateComputerChoice();
+  };
+
+  const generateComputerChoice = () => {
+    const num = Math.floor(Math.random() * data.length);
+    setComputerItem(data[num]);
+  };
+
+  const determineScore = (myItem, computerItem) => {
+    if (myItem.name === computerItem.name) {
+      setText("It's a tie");
+    } else if (
+      (myItem.name === "rock" && computerItem.name === "scissors") ||
+      (myItem.name === "paper" && computerItem.name === "rock") ||
+      (myItem.name === "scissors" && computerItem.name === "paper")
+    ) {
+      setMyScore((prevScore) => prevScore + 1);
+      setText("Player wins");
+    } else {
+      setComputerScore((prevScore) => prevScore + 1);
+      setText("Computer wins");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        
+    {clicked && <h2 className="textInfo">{text}</h2>}
+      
+
+    <div className="resultContainer">
+     
+        <img
+          src={myItem.source}
+          style={{ transform: "rotateY(180deg) rotate(-90deg)",display:clicked?"inline-block":"none" }}
+          alt="alternate"
+          width="50"
+        />
+      
+      
+        <img
+          src={computerItem.source}
+          style={{ transform: "rotate(-90deg)",display:clicked?"inline-block":"none"  }}
+          alt="alternate"
+          width="50"
+        />
+
+     
+      </div>
+
+     <div className="scoreContainer">
+
+    <h2>{myScore}</h2>
+    <h2>{computerScore}</h2>
+
+      </div>
+
+    <div className="container">
+      {data.map((item, index) => (
+        <img
+          key={index}
+          src={item.source}
+          onClick={() => handleClick(item)}
+          alt="alternative"
+          width="50"
+          height="50"
+        />
+      ))}
     </div>
+
+    </div>
+
   );
 }
 
---
+++ src/Data.js
@@ -0,0 +1,6 @@
+const data = [
+    {name:"rock",source:"images for game/rock.png"},
+    {name:"paper",source:"images for game/paper.png"},
+    {name:"scissors",source:"images for game/scissors.png"}
+];
+export default data;
